23030818f14b15e90cb2c5cd186fb0e97e26720c
Leonardo HP <ba.leonardohp@gmail.com>
2022-06-15 01:30:13 -0500

    pelea

--- Rol.js
+++ Rol.js
@@ -29,21 +29,59 @@ alert( "Con que tu nombre es "+ heroe1.nombre + " ? que curioso nombre, pero qui
 //--------------------------------------------------------------------
 const inventario = []
 
+
+
 class Consumible {
-  constructor(cantidadMejora, tipoMejora, valor) {
+  constructor(id, nombreConsumible, cantidadMejora, tipoMejora, valor) {
+    this.id = id
+    this.nombreConsumible = nombreConsumible
     this.cantidadMejora = cantidadMejora
     this.tipoMejora = tipoMejora
     this.valor = valor
   }
 }
 
-const pastelDeManzana = new Consumible(10,"vida",10)
-const vinoTinto = new Consumible(10,"fuerza",10)
+//Objetos Consumibles
+//---------------------------------------------------------------------
+// const consumibles = [
+//  Consumible(101,10,"pastelDeManzana1","vida",10),
+//  Consumible(102,10,"pocionDeFuerza","fuerza",20),
+//  Consumible(103,2,"pocionDeFuerza","fuerza",50),
+//  Consumible(104,2,"cangrejoAsado","vida",10),
+//  Consumible(105,10,"RataAsada","vida",8),
+// ]
+//---------------------------------------------------------------------
+//Lista Enemigos
+//---------------------------------------------------------------------
+const enemigos = [];
+
 
 //---------------------------------------------------------------------
 
+function RecibirPastelDeManzana(){
+  inventario.push(new Consumible(101,10,"pastelDeManzana","vida",10))
+}
+debugger
 
+function pelea(){
 
+  if (confirm("usar un dado?")) {
+   let fuerzaAventurero = Math.random(0)* 20
+  }
+  else {
+  let fuerzaAventurero = heroe1.fuerza
+  }
+
+if (fuerzaAventurero >= 10){
+  
+  heroe1.oro += 2
+    alert("Ganaste la pelea, ganas 2 de oro")
+  }
+  else {
+    
+    alert("Quedas noqueado en el suelo sin saber que paso, pierdes 2 de oro")
+  }
+}
 
 debugger
 
@@ -66,7 +104,7 @@ switch (respuesta1){
 
   case 3:
     alert("Bueno normalmente no hago muchas caridades pero por tu aspecto comprare ese karma...") 
-    inventario.push(pastelDeManzana)
+    RecibirPastelDeManzana()
     alert ("RECIBES PASTEL DE MANZANA") 
     alert("Bueno y ahora que probaste bocado, tal vez podrias hacer algo por nosotros a cambio...")
     
@@ -80,10 +118,35 @@ primerDialogo()
 }
 }
 
+debugger
+
 function respuestaOro(){
     let respuesta2 = parseInt(prompt("Tal vez no tengamos mucho pero si nos ayudas tal vez podriamos recompensarte, que dices "+ heroe1.nombre + " ? \n 1 acepto \n 2 No me interesa \n 3 quiero pelear!! " ));
+
+
+switch (respuesta2){
+
+  case 1:
+  alert("Excelente! pues dejame contarte de que trata el asunto... ") 
+  break;
+
+case 2:
+alert("Es tu decision...pero no habra mucho que hacer por aqui...apenas estan maquetando los dialogos")
+respuestaOro()
+  break;
+
+case 3:
+  alert("...ok.. creo que es hora de que alguien te muetre modales!! Alza los puños") 
+   pelea()
+  break;
+
 }
 
+if (respuesta2 > 3) { 
+alert("por favor escoge una de las opciones disponibles");
+respuesta2()
+}
+}
 
 
 
