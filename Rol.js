1edc76fdf266bfd81fbd69b317aad3ff2024601d
Leonardo HP <ba.leonardohp@gmail.com>
2022-06-16 00:25:40 -0500

    formato

--- Rol.js
+++ Rol.js
@@ -1,6 +1,73 @@
-
-
 debugger
+//INVENTARIO 
+//--------------------------------------------------------------------
+const inventario = []
+
+//Objetos Consumibles
+//---------------------------------------------------------------------
+
+class Consumible {
+  constructor(id, nombreConsumible, cantidadMejora, tipoMejora, valor) {
+    this.id = id
+    this.nombreConsumible = nombreConsumible
+    this.cantidadMejora = cantidadMejora
+    this.tipoMejora = tipoMejora
+    this.valor = valor
+  }
+}
+const consumibles = [
+new Consumible(101,10,"pastelDeManzana1","vida",10),
+new Consumible(102,10,"pocionDeFuerza","fuerza",20),
+new Consumible(103,2,"pocionDeFuerza","fuerza",50),
+new Consumible(104,2,"cangrejoAsado","vida",10),
+new Consumible(105,10,"RataAsada","vida",8),
+]
+//-----------------------------------------------------------------------
+
+//Lista Enemigos
+//---------------------------------------------------------------------
+const enemigos = [];
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------
+
+// Pelea
+function pelea(){
+  debugger
+  let dado = (confirm("usar un dado?"))
+  
+    if (dado == true ) {
+   fuerzaAventurero = (Math.random(0)* 20)
+    }
+    else {
+  fuerzaAventurero = (heroe1.fuerza)
+    }
+  
+  if (fuerzaAventurero >= 10){
+     heroe1.oro -= 2
+      alert("Quedas noqueado en el suelo sin saber que paso, pierdes 2 de oro")
+  }
+  
+    else {
+      heroe1.oro += 2
+      alert("Quedas noqueado en el suelo sin saber que paso, pierdes 2 de oro")
+  }
+  
+  }
+  
+
+//-----------------------------------------------------------------------------
+
+
+
+
+
 alert ("tras una honesta pero tranquila vida de herrero, creando armaduras para grandes caballeros desde la comodidades de tu taller \n Un dia decides que es momento de crear tus propias aventuras, vendes tu taller compras un caballo y te lanzas a la aventura.  \n \n sin realmente saber lo que estas haciendo, vas a caballo hacia donde tu instinto te guia... ")
 alert ("Despues de cabalgar un par de dias, te encuentras con una aldea \n Siguiendo tu estomago te encuentras con una taverna, de la cual te recibe un un hombre corpulento, dueño del lugar, mirandote con extrañesa... ")
 debugger
@@ -18,72 +85,17 @@ class Aventurero{
 
 const heroe1 = new Aventurero( nombreAventurero , 20 , 100, 10 ) 
 
-
-
-
 alert( "Con que tu nombre es "+ heroe1.nombre + " ? que curioso nombre, pero quien soy yo para juzgar?" );
 
-
-//INVENTARIO 
-//--------------------------------------------------------------------
-const inventario = []
-
-
-
-class Consumible {
-  constructor(id, nombreConsumible, cantidadMejora, tipoMejora, valor) {
-    this.id = id
-    this.nombreConsumible = nombreConsumible
-    this.cantidadMejora = cantidadMejora
-    this.tipoMejora = tipoMejora
-    this.valor = valor
-  }
-}
-
-//Objetos Consumibles
-//---------------------------------------------------------------------
-// const consumibles = [
-//  Consumible(101,10,"pastelDeManzana1","vida",10),
-//  Consumible(102,10,"pocionDeFuerza","fuerza",20),
-//  Consumible(103,2,"pocionDeFuerza","fuerza",50),
-//  Consumible(104,2,"cangrejoAsado","vida",10),
-//  Consumible(105,10,"RataAsada","vida",8),
-// ]
-//---------------------------------------------------------------------
-//Lista Enemigos
-//---------------------------------------------------------------------
-const enemigos = [];
-
-
 //---------------------------------------------------------------------
 
-function RecibirPastelDeManzana(){
-  inventario.push(new Consumible(101,10,"pastelDeManzana","vida",10))
-}
+
+
+
 debugger
 
-function pelea(){
-debugger
-let dado = (confirm("usar un dado?"))
 
-  if (dado == true ) {
- fuerzaAventurero = (Math.random(0)* 20)
-  }
-  else {
-fuerzaAventurero = (heroe1.fuerza)
-  }
 
-if (fuerzaAventurero >= 10){
-   heroe1.oro -= 2
-    alert("Quedas noqueado en el suelo sin saber que paso, pierdes 2 de oro")
-}
-
-  else {
-    heroe1.oro += 2
-    alert("Quedas noqueado en el suelo sin saber que paso, pierdes 2 de oro")
-}
-
-}
 
 
 debugger
@@ -107,7 +119,7 @@ switch (respuesta1){
 
   case 3:
     alert("Bueno normalmente no hago muchas caridades pero por tu aspecto comprare ese karma...") 
-    RecibirPastelDeManzana()
+    inventario.push(consumibles.find(item => item.id == 101));
     alert ("RECIBES PASTEL DE MANZANA") 
     alert("Bueno y ahora que probaste bocado, tal vez podrias hacer algo por nosotros a cambio...")
     
