867b63b7ae9d5bc07641033d922826d94a42596a
Leonardo HP <ba.leonardohp@gmail.com>
2022-06-29 23:09:56 -0500

    Nombre

--- Rol.js
+++ Rol.js
@@ -5,44 +5,44 @@
 // //Objetos Consumibles
 // //---------------------------------------------------------------------
 
-// class Consumible {
-//   constructor(id, nombreConsumible, cantidadMejora, tipoMejora, valor) {
-//     this.id = id
-//     this.nombreConsumible = nombreConsumible
-//     this.cantidadMejora = cantidadMejora
-//     this.tipoMejora = tipoMejora
-//     this.valor = valor
-//   }
-// }
-// //--------------------------------------------------------------------
-// const consumibles = [
-// new Consumible(101,"pastelDeManzana1",10,"vida",10),
-// new Consumible(102,"pocionDeFuerza",10,"fuerza",20),
-// new Consumible(103,"pocionDeFuerza",2,"fuerza",50),
-// new Consumible(104,"cangrejoAsado",2,"vida",10),
-// new Consumible(105,"RataAsada",10,"vida",8),
-// ]
+class Consumible {
+  constructor(id, nombreConsumible, cantidadMejora, tipoMejora, valor) {
+    this.id = id
+    this.nombreConsumible = nombreConsumible
+    this.cantidadMejora = cantidadMejora
+    this.tipoMejora = tipoMejora
+    this.valor = valor
+  }
+}
+//--------------------------------------------------------------------
+const consumibles = [
+new Consumible(101,"pastelDeManzana1",10,"vida",10),
+new Consumible(102,"pocionDeFuerza",10,"fuerza",20),
+new Consumible(103,"pocionDeFuerza",2,"fuerza",50),
+new Consumible(104,"cangrejoAsado",2,"vida",10),
+new Consumible(105,"RataAsada",10,"vida",8),
+]
 // //-----------------------------------------------------------------------
 
 // //Lista Enemigos
 // //---------------------------------------------------------------------
 
-//   class Enemigo {
-//     constructor(id, nombreEnemigo, fuerzaEnemigo, drop) {
-//       this.id = id
-//       this.nombreEnemigo = nombreEnemigo
-//       this.fuerzaEnemigo = fuerzaEnemigo
-//       this.drop = drop
-//     }
-//   }
-// //-----------------------------------------------------------------------
-// const enemigos =[
-// new Enemigo(106,"Tavernero",10,10),
-// new Enemigo(107,"Lobo",10,10),
-// new Enemigo(108,"Goblin",10,10),
-// new Enemigo(109,"ardillaEnojada",10,10),
-// new Enemigo(110,"elProfeFerLuna",10,10),
-// ]
+  class Enemigo {
+    constructor(id, nombreEnemigo, fuerzaEnemigo, drop) {
+      this.id = id
+      this.nombreEnemigo = nombreEnemigo
+      this.fuerzaEnemigo = fuerzaEnemigo
+      this.drop = drop
+    }
+  }
+//-----------------------------------------------------------------------
+const enemigos =[
+new Enemigo(106,"Tavernero",10,10),
+new Enemigo(107,"Lobo",10,10),
+new Enemigo(108,"Goblin",10,10),
+new Enemigo(109,"ardillaEnojada",10,10),
+new Enemigo(110,"elProfeFerLuna",10,10),
+]
 
 
 
@@ -51,7 +51,6 @@
 // // inventario.push(consumibles.find(item => item.id == 101));
 
 // //! DIALOGOS---------------------------------------------------------------------------------------------------------
-
 
 class Parrafo {
   constructor(id, parrafo) {
@@ -63,8 +62,12 @@ class Parrafo {
 const parrafos = [
 new Parrafo(001,"Tras una honesta pero tranquila vida de herrero, creando armaduras para grandes caballeros desde la comodidades de tu taller \n Un dia decides que es momento de crear tus propias aventuras, vendes tu taller compras un caballo y te lanzas a la aventura.  \n \n sin realmente saber lo que estas haciendo, vas a caballo hacia donde tu instinto te guia... "),
 new Parrafo(002,"Despues de cabalgar un par de dias, te encuentras con una aldea \n Siguiendo tu estomago te encuentras con una taverna, de la cual te recibe un un hombre corpulento, dueño del lugar, mirandote con extrañesa... "),
-new Parrafo(003,"Saludos viajero! cual es tu nombre?")
+new Parrafo(003,"Saludos viajero! cual es tu nombre?"),
+new Parrafo(004,"Con que tu nombre es"),
+new Parrafo(005,". . . que curioso nombre, pero quien soy yo para juzgar?")
 ]
+
+
 
 
 // //! PELEA---------------------------------------------------------------
@@ -119,6 +122,40 @@ new Parrafo(003,"Saludos viajero! cual es tu nombre?")
 // }
 
 
+function resetCounter() {
+  counterVal = 0;
+  updateDisplay(counterVal);
+}
+
+var counterVal = 0;
+
+function incrementClick() {
+    updateDisplay(++counterVal);
+}
+
+function updateDisplay(counterVal) {
+   
+    switch (counterVal) {
+      case (1):
+      document.getElementById("titulo").innerHTML = parrafos.find(item => item.id == 001).parrafo;
+      break
+
+      case(2):
+      document.getElementById("titulo").innerHTML = parrafos.find(item => item.id == 002).parrafo;
+      break
+
+      case(3):
+      document.getElementById("titulo").innerHTML = parrafos.find(item => item.id == 003).parrafo;
+      break
+
+      // aqui se pregunta el nombre 
+
+      case(4):
+      document.getElementById("titulo").innerHTML = parrafos.find(item => item.id == 004).parrafo + ;
+break
+
+    }
+}
 
 
 hacerDialogo()
@@ -129,11 +166,13 @@ function seEscucha(){
 }
 
 function hacerDialogo() {
-  if (document.querySelector("button").value = "-") {
+
 let continuar = document.querySelector("button")
-continuar.addEventListener("click",()=> seEscucha())
+continuar.addEventListener("click",()=> (seEscucha(), incrementClick() ) )
 }
-}
+
+
+
 
 
 
@@ -141,11 +180,11 @@ continuar.addEventListener("click",()=> seEscucha())
 
 
 
-function resultado(){
-  let buscar = "001"
-let resultado = parrafos.find((parrafo)  => parrafo.id === "001");
-console.log(resultado)
-}
+// function resultado(){
+//   let buscar = "001"
+// let resultado = parrafos.find((parrafo)  => parrafo.id === "001");
+// console.log(resultado)
+// }
 
 
 
@@ -156,39 +195,19 @@ console.log(resultado)
 
 
 
-// debugger
-// texto1()
-// function texto1(){
-// let titulo = document.getElementById("titulo")
-// titulo.innerText ="Tras una honesta pero tranquila vida de herrero, creando armaduras para grandes caballeros desde la comodidades de tu taller \n Un dia decides que es momento de crear tus propias aventuras, vendes tu taller compras un caballo y te lanzas a la aventura.  \n \n sin realmente saber lo que estas haciendo, vas a caballo hacia donde tu instinto te guia... "
-
-// document.getElementById("continuar").onclick = texto2()
-// }
-
-
-// function texto2(){
-//   let titulo = document.getElementById("titulo")
-//   titulo.innerText ="Despues de cabalgar un par de dias, te encuentras con una aldea \n Siguiendo tu estomago te encuentras con una taverna, de la cual te recibe un un hombre corpulento, dueño del lugar, mirandote con extrañesa... "  
-
-// }
 
 
 
-// alert ("Despues de cabalgar un par de dias, te encuentras con una aldea \n Siguiendo tu estomago te encuentras con una taverna, de la cual te recibe un un hombre corpulento, dueño del lugar, mirandote con extrañesa... ")
-// debugger
+class Aventurero{
+  constructor(nombre, oro, vida, fuerza){
+  this.nombre = nombre
+  this.oro = oro
+  this.vida = vida
+  this.fuerza = fuerza
+}
+}
 
-// let nombreAventurero = prompt("Saludos viajero! cual es tu nombre?")
-
-// class Aventurero{
-//   constructor(nombre, oro, vida, fuerza){
-//   this.nombre = nombre
-//   this.oro = oro
-//   this.vida = vida
-//   this.fuerza = fuerza
-// }
-// }
-
-// const heroe1 = new Aventurero( nombreAventurero , 20 , 10, 10 ) 
+const heroe1 = new Aventurero( nombreAventurero , 20 , 10, 10 ) 
 
 // alert( "Con que tu nombre es "+ heroe1.nombre + " ? que curioso nombre, pero quien soy yo para juzgar?" );
 
